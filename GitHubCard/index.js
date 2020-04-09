be5c8bbafdb280e116a9cb3d2d7ef8b2f543a001
ToEndThePeace <bran.ramirez.don@gmail.com>
2020-04-09 13:57:24 -0700

    Added functionality to click a button to load git users from data array, as well as a form to pull users from the api based on username entered. Also added a close button to oepn cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,50 +1,40 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
+const gitUserData = ["ToEndThePeace", "sadamexx", "shayne-smith", "Istott"];
+
+// Simple Utility Functions
 function get(selector) {
   return document.querySelectorAll(selector);
 }
 function make(htmlTag) {
   return document.createElement(htmlTag);
 }
-function getGit(gitHandle) {
-  axios
-    .get(`https://api.github.com/users/${gitHandle}`)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+
+/**
+ * @name getGitFromList 
+ * @summary Pulls data from GitHub's user API and calls makeCard() on it
+ * @param {string[]} gitArray Is an array of valid GitHub handles
+ */
+function getGitFromList(gitArray) {
+  gitArray.forEach((x) => {
+    axios
+      .get(`https://api.github.com/users/${x}`)
+      .then((res) => {
+        makeCard(res.data);
+      })
+      .catch((err) => {
+        alert("ERROR: Request failed.\n" + err);
+      });
+  });
 }
 
-/*  Step 2: Inspect and study the data coming back, this is YOUR 
-      github info! You will need to understand the structure of this 
-      data in order to use it to build your component function 
-
-      Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
-const followersArray = [];
-
-function compGitCard(gitObj) {
+/**
+ * @name makeCard
+ * @summary creates an HTML node from a GitHub User object and adds to the DOM
+ * @param {Object} gitData is a GitHub user's complte information pulled from their API
+ */
+function makeCard(gitData) {
   // Create HTML Elements
   const card = make("div");
+  const close = make("span");
   const image = make("img");
   const info = make("div");
   const name = make("h3");
@@ -59,60 +49,56 @@ function compGitCard(gitObj) {
 
   // Apply Attributes and Content
   card.classList.add("card");
-  image.src = gitObj.data.avatar_url;
+  close.classList.add("close");
+  close.innerHTML = "&times;";
+  image.setAttribute("src", gitData.avatar_url);
   info.classList.add("card-info");
   name.classList.add("name");
-  name.textContent = gitObj.data.name;
+  name.textContent = gitData.name;
   username.classList.add("username");
-  username.textContent = gitObj.data.login;
+  username.textContent = gitData.login;
   location.textContent =
-    "Location: " + (gitObj.data.location ? gitObj.data.location : "Unknown");
-  link.href = gitObj.data.html_url;
-  link.textContent = gitObj.data.html_url;
-  followers.textContent = `Followers: ${gitObj.data.followers}`;
-  following.textContent = `Following: ${gitObj.data.following}`;
-  bio.textContent = `Bio: ${gitObj.data.bio}`;
+    "Location: " + (gitData.location ? gitData.location : "Unknown");
+  link.href = gitData.html_url;
+  link.textContent = gitData.html_url;
+  followers.textContent = `Followers: ${gitData.followers}`;
+  following.textContent = `Following: ${gitData.following}`;
+  bio.textContent = `Bio: ${gitData.bio}`;
 
   //Structure the Element
+  card.appendChild(close);
   card.appendChild(image);
   card.appendChild(info);
   info.appendChild(name);
   info.appendChild(username);
   info.appendChild(location);
-  info.appendChild(profileText);
-  info.appendChild(link);
+  profile.appendChild(profileText);
+  profile.appendChild(link);
+  info.appendChild(profile);
   info.appendChild(followers);
   info.appendChild(following);
   info.appendChild(bio);
 
-  //TODO Event Listeners
+  // Event Listeners
+  close.addEventListener("click", () => {
+    card.classList.add("noshow");
+  })
 
-  //TODO Return New Card Node
+  // Add New Card Node to the DOM
+  get(".cards")[0].appendChild(card);
 }
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+const loadButton = get("#loadUsers")[0];
+loadButton.addEventListener("click", () => {
+  getGitFromList(gitUserData);
+  loadButton.disabled = true;
+  loadButton.textContent = "Users Loaded";
+})
 
-*/
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+const submitButton = get("#submit")[0];
+submitButton.addEventListener("click", () => {
+  const textField = get("#search")[0];
+  const val = textField.value;
+  textField.value = "";
+  getGitFromList([val]);
+})
